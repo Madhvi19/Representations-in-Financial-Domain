65d70ea4c6f768ac5d7197f63f6d066f0d0ff16d
ShivprasadSagare <shivprasad.sagare@gmail.com>
2020-10-17 19:15:52 +0530

    hyperparameters as arguments from command line

--- glove/glove_script.py
+++ glove/glove_script.py
@@ -3,16 +3,16 @@ import os
 import re
 import sys
 path = sys.argv[1]
-freq = sys.argv[2]
-window = sys.argv[3]
-dim = sys.argv[4]
-lr = sys.argv[5]
-epochs = sys.argv[6]
+freq = int(sys.argv[2])
+window = int(sys.argv[3])
+dim = int(sys.argv[4])
+lr = float(sys.argv[5])
+epochs = int(sys.argv[6])
 
 def train(path, freq, window, dim, lr, epochs):
     lines = []
     dic = {}
-    print("Start.......")
+    print("Start of train method")
     try:
         for f in os.listdir(path):
             text = open(path + '/' + f, 'r').read()
@@ -23,21 +23,21 @@ def train(path, freq, window, dim, lr, epochs):
                     dic[word] += 1
                 else:
                     dic[word] = 1
-        print("Creating dictionary with counts of words is done!!!!")
+        print("Created Dictionary for frequencies of words.")
         for f in os.listdir(path):
             text = open(path + '/' + f, 'r').read()
             text = re.sub('\n', ' ', text)
             text = text.split()
             text = [word for word in text if dic[word]>freq]
             lines.append(text)
-        print("Creating array of list of files is done!!!!")
+        print("Converted preprocessed text data in input format of array of array of words.")
         corpus = Corpus()
         corpus.fit(lines, window=window)
         glove = Glove(no_components=dim, learning_rate=lr)
         glove.fit(corpus.matrix, epochs=epochs, verbose=True)
         glove.add_dictionary(corpus.dictionary)
         glove.save('glove.model')
-        print("Saved the trained model to glove.model!!!!")
+        print("Saved the trained model to glove.model.")
     except:
         print("Error occured in training glove model")
 
